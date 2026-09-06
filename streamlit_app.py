260de8cfb4a04a5b2926b0af493cb001e607d6ea
pleusicles-code <kozakdaniel@gmail.com>
2026-09-06 18:26:52 +0200

    Remove temporary re-login sidebar button

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,4 @@
 
 import runpy
 
-import streamlit as st
-
-if st.user.is_logged_in and st.sidebar.button("Log out and sign in again"):
-    st.logout()
-
 runpy.run_path("latin_morph.py", run_name="__main__")
